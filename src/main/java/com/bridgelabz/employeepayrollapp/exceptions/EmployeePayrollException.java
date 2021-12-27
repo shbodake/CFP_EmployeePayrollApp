5a4babc8eabf7ad8961aba1f3ee538cec163c19f
shbodake <sayalibodake28@gmail.com>
2021-12-27 18:21:39 +0530

    Added: Exception handling

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollException.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.employeepayrollapp.exceptions;
+
+/**
+ * Creating custom Exception
+ */
+public class EmployeePayrollException extends RuntimeException {
+	public EmployeePayrollException(String message) {
+		super(message);
+	}
+
+}
